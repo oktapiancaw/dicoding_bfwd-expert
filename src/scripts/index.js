764120265fb349e0e363cf047cefad769cdfcfa8
oktapiancaw <oktapian1998@gmail.com>
2021-06-09 18:44:20 +0700

    Basic App

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,10 @@
-import 'regenerator-runtime';
+import 'regenerator-runtime'
+import '../styles/style.css'
+import '../styles/responsive.css'
+import App from './views/app'
 
-// eslint-disable-next-line no-console
-console.log('Hello Coders!');
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent')
+})
