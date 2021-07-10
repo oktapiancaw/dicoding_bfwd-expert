ac1f780f6b1679d6d21f9fc47033a437862ee6fb
oktapiancaw <oktapian1998@gmail.com>
2021-07-10 20:02:12 +0700

    clean unused file

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,15 +1,11 @@
-import AboutPage from '../views/pages/about'
 import DetailPage from '../views/pages/detail'
 import FavoritePage from '../views/pages/favorite'
 import HomePage from '../views/pages/home'
-import ListPage from '../views/pages/list'
 
 const routes = {
   '/': HomePage,
   '/home': HomePage,
   '/favorite': FavoritePage,
-  '/list': ListPage,
-  '/about': AboutPage,
   '/detail/:id': DetailPage
 }
 export default routes
--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,11 +1,36 @@
+import FavoriteRestaIdb from '../../data/favoriteresta-idb'
+import { createRestoCard } from '../templates/card'
+
 const FavoritePage = {
   async render () {
     return `
-      <h2>Your Favorite</h2>
+    <div class="content padx-4 pady-3">
+      <h5 class="content__heading">Favorite Restaurant & Cafe </h5>
+      <div class="content__box padx-2" id="card_container">
+        <div class="card shadow skeleton" id="skeleton_card">
+          <img class="card__img_head" />
+          <div class="card__detail"> 
+            <div>
+            </div>
+            <div>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
     `
   },
   async afterRender () {
-    //
+    const resto = await FavoriteRestaIdb.getAllRestas()
+    const cardContainer = document.querySelector('#card_container')
+    const skeleton = document.querySelector('#skeleton_card')
+    if (resto) {
+      skeleton.remove()
+    }
+    resto.forEach((data) => {
+      cardContainer.innerHTML += createRestoCard(data)
+    })
   }
 }
 export default FavoritePage
